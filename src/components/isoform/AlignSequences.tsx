2036c290b0a669f4bfae00c90a0ffe30322c0757
Daniel Edler <edler.dev@gmail.com>
2024-08-19 23:27:16 +0200

    feat: Show sequence sizes

--- src/components/isoform/AlignSequences.tsx
+++ src/components/isoform/AlignSequences.tsx
@@ -1,4 +1,17 @@
-import { Box, Button, Code, Flex, Heading } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Code,
+  Flex,
+  Heading,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
 import { useContext } from "react";
 import { StoreContext } from "../../store";
 import { getAAColor } from "../../store/PdbStore";
@@ -6,7 +19,7 @@ import { observer } from "mobx-react";
 
 export default observer(function AlignSequences({}: {}) {
   const store = useContext(StoreContext);
-  const { alignment } = store.input;
+  const { alignment, isoforms } = store.input;
 
   return (
     <Flex
@@ -17,18 +30,37 @@ export default observer(function AlignSequences({}: {}) {
     >
       <p>Load or calculate a sequence alignment of the isoforms.</p>
 
-      <Flex mt={8}>
-        <Box>
-          {/* {alignment.map((item) => (
-            <Box key={`${item.name}-id`}>{item.name}</Box>
-          ))} */}
-          <Button
-            isDisabled={!store.input.canGenerateAlignment}
-            onClick={store.input.generateAlignment}
-          >
-            Align
-          </Button>
-        </Box>
+      <Flex direction="column" mt={8} alignItems="center">
+        <TableContainer mb={2}>
+          <Table variant="simple" size="sm">
+            <Thead>
+              <Tr>
+                <Th>Sequence</Th>
+                <Th isNumeric>Size</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td>Isoform 1</Td>
+                <Td isNumeric>{isoforms[0].sequence?.code.length ?? 0}</Td>
+              </Tr>
+              <Tr>
+                <Td>Isoform 2</Td>
+                <Td isNumeric>{isoforms[1].sequence?.code.length ?? 0}</Td>
+              </Tr>
+              <Tr>
+                <Td>Alignment</Td>
+                <Td isNumeric>{alignment[0].sequence.length}</Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </TableContainer>
+        <Button
+          isDisabled={!store.input.canGenerateAlignment}
+          onClick={store.input.generateAlignment}
+        >
+          Align
+        </Button>
 
         <Box overflowX="scroll" maxW={800}>
           {alignment.map(({ name, sequence }) => (
