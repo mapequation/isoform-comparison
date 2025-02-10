9bee889af0da8ccb692ead59805b91b029e65a4a
Daniel Edler <edler.dev@gmail.com>
2025-02-10 16:18:08 +0100

    feat: Highlight low-confidence nodes in Alluvial diagram
    Define a node as insignificant if the average pLDDT score, encoded as temperature in pdb files, is below 70.

--- src/store/PdbStore.ts
+++ src/store/PdbStore.ts
@@ -9,6 +9,7 @@ import { calcStatistics } from "../components/LoadNetworks/utils"
 import IsoformStore from "./IsoformStore";
 import { runInfomap } from "../utils/infomap";
 import { ErrorItem } from "./InputStore";
+import { mean } from "d3";
 
 const aaMap = new Map([
     ["ALA", "A"], ["ARG", "R"], ["ASN", "N"], ["ASP", "D"], ["CYS", "C"], ["GLN", "Q"], ["GLU", "E"], ["GLY", "G"], ["HIS", "H"],
@@ -54,6 +55,7 @@ type PdbItem = {
     pos: number;
     aa: string;
     coords: [Coord, ...Coord[]]; // Minimum one coordinate
+    confidences: number[];
 }
 
 export type IsoformNode = {
@@ -66,6 +68,7 @@ export type IsoformNode = {
     fx?: number;
     fy?: number;
     fz?: number;
+    confidence: number;
 }
 
 export type IsoformLink = {
@@ -248,47 +251,77 @@ export default class PdbStore {
         }
     })
 
+    /**
+     * PDB file format:
+     * 
+Contains:	the atomic coordinates for standard residues and the occupancy and temperature factor for each atom
+
+COLUMNS        DATA TYPE       CONTENTS                            
+--------------------------------------------------------------------------------
+ 1 -  6        Record name     "ATOM  "                                            
+ 7 - 11        Integer         Atom serial number.                   
+13 - 16        Atom            Atom name.                            
+17             Character       Alternate location indicator.         
+18 - 20        Residue name    Residue name.                         
+22             Character       Chain identifier.                     
+23 - 26        Integer         Residue sequence number.              
+27             AChar           Code for insertion of residues.       
+31 - 38        Real(8.3)       Orthogonal coordinates for X in Angstroms.                       
+39 - 46        Real(8.3)       Orthogonal coordinates for Y in Angstroms.                            
+47 - 54        Real(8.3)       Orthogonal coordinates for Z in Angstroms.                            
+55 - 60        Real(6.2)       Occupancy.                            
+61 - 66        Real(6.2)       Temperature factor (Default = 0.0).                   
+73 - 76        LString(4)      Segment identifier, left-justified.   
+77 - 78        LString(2)      Element symbol, right-justified.      
+79 - 80        LString(2)      Charge on the atom.       
+
+Example: 
+
+         1         2         3         4         5         6         7         8
+12345678901234567890123456789012345678901234567890123456789012345678901234567890
+ATOM    145  N   VAL A  25      32.433  16.336  57.540  1.00 11.92      A1   N
+ATOM    146  CA  VAL A  25      31.132  16.439  58.160  1.00 11.85      A1   C
+ATOM    147  C   VAL A  25      30.447  15.105  58.363  1.00 12.34      A1   C
+ATOM    148  O   VAL A  25      29.520  15.059  59.174  1.00 15.65      A1   O
+ATOM    149  CB AVAL A  25      30.385  17.437  57.230  0.28 13.88      A1   C
+ATOM    150  CB BVAL A  25      30.166  17.399  57.373  0.72 15.41      A1   C
+     * 
+     * 
+     * @param content .pdb file content
+     * @param filename optional filename for error messages
+     */
     parsePdbContent = async (content: string, filename?: string) => {
         if (filename === undefined) {
             filename = "loaded content"
         }
         const lines = content.split("\n");
-        const re = /^ATOM\s+\d+\s+(?<atom>\w+)\s+(?<aa>\w+)\s+\w\s*(?<pos>\d+)\s+(?<x>-?\d+\.\d+)\s+(?<y>-?\d+\.\d+)\s+(?<z>-?\d+\.\d+)\s+/;
         for (let i = 0; i < lines.length; ++i) {
             const line = lines[i];
             if (line.substring(0, 4) !== "ATOM") {
                 continue;
             }
-
-            const match = re.exec(line);
-            if (match === null || match.groups === undefined) {
-                throw Error(`Line ${i + 1} (${line}) of ${filename} doesn't match expected pattern.`);
+            if (line.length < 66) {
+                throw Error(`Line ${i + 1} (${line}) of ${filename} doesn't match the format for ATOM records.`);
             }
-            const atom = match.groups.atom;
+
+            const atom = line.substring(12, 16).trim();
             if (atom !== "CA") {
                 continue;
             }
-            const aa = aaMap.get(match.groups.aa)!;
-            const pos = Number(match.groups.pos)
-            const x = Number(match.groups.x);
-            const y = Number(match.groups.y);
-            const z = Number(match.groups.z);
-
-            // const items = line.split(/\s+/);
-            // if (items[0] !== "ATOM" || items[2] !== "CA") continue;
-
-            // const aa = aaMap.get(items[3])!;
-            // const pos = Number(items[5])
-            // const x = Number(items[6]);
-            // const y = Number(items[7]);
-            // const z = Number(items[8]);
+            const residue = line.substring(17, 20).trim();
+            const aa = aaMap.get(residue)!;
+            const pos = Number(line.substring(22, 26).trim());
+            const x = Number(line.substring(30, 38).trim());
+            const y = Number(line.substring(38, 46).trim());
+            const z = Number(line.substring(46, 54).trim());
+            const confidence = Number(line.substring(60, 66).trim());
 
             if (pos < 0 || pos !== Math.round(pos)) {
                 throw Error(`Position ${pos} not valid in line '${line}' of ${filename}.`)
             }
 
             if (this.numDatasets === 0) {
-                this.data.set(pos, { aa, pos, coords: [[x, y, z]] })
+                this.data.set(pos, { aa, pos, coords: [[x, y, z]], confidences: [confidence] })
             }
             else {
                 const item = this.data.get(pos);
@@ -299,6 +332,7 @@ export default class PdbStore {
                     throw Error(`Aminoacid '${aa}' in pos ${pos} in dataset ${this.numDatasets + 1} does not match '${item.aa}' in previous dataset`);
                 }
                 item.coords.push([x, y, z]);
+                item.confidences.push(confidence);
             }
         }
         // console.log("pdb parsed data:", this.data)
@@ -351,6 +385,7 @@ export default class PdbStore {
 
         this.data.forEach((item, index) => {
             const [fx, fy, fz] = item.coords[this.selectedIndex];
+            const confidence = mean(item.confidences)!;
             nodes.push({
                 index,
                 id: `${item.pos}`,
@@ -358,6 +393,7 @@ export default class PdbStore {
                 color: aaColorMap.get(item.aa)!,
                 pdbItem: item,
                 fx, fy, fz,
+                confidence,
             })
         });
         return nodes;
@@ -420,6 +456,14 @@ export default class PdbStore {
     })
 
     setProgress = action((progress: number) => { this.infomap.progress = progress })
+
+    isInsignificantValue(confidence: number) {
+        return confidence < 70;
+    }
+
+    isInsignificant(node: IsoformNode) {
+        return this.isInsignificantValue(node.confidence);
+    }
 
     getModuleColor(module?: number) {
         const defaultColor = "#cccccc";
@@ -497,12 +541,20 @@ export default class PdbStore {
 
         const modules = new Map<string, number>();
 
+        const confidenceMap = new Map<string, number>();
+        this.network.nodes.forEach(node => {
+            confidenceMap.set(node.id, node.confidence);
+        })
+
         console.log(`Infomap result for '${this.name}':`, netFile);
 
         netFile.nodes.forEach((node) => {
             const path = Array.isArray(node.path) ? node.path : node.path?.split(":") ?? [];
             const topModule = Number(path[0]);
             modules.set(`${node.id}`, topModule);
+            if (this.isInsignificantValue(confidenceMap.get(`${node.id}`)!)) {
+                node.path = path.join(";") + ";";
+            }
         })
 
         this.network.nodes.forEach(node => {
